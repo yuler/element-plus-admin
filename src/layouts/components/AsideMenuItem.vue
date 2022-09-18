d0c6921a02fec32414b14c8605c1afafa28c5d85
yuler <is.yuler@gmail.com>
2022-09-19 00:00:38 +0800

    feat: add `router-link` wrap `el-menu-item` in AsideMenuItem

--- src/layouts/components/AsideMenuItem.vue
+++ src/layouts/components/AsideMenuItem.vue
@@ -9,10 +9,19 @@ defineProps<{
 </script>
 
 <template>
-  <el-sub-menu ref="subMenu" :index="item.path">
+  <template v-if="!item.children">
+    <router-link :to="item.path">
+      <el-menu-item :index="item.path">
+        <template #title>
+          {{ item.meta?.title }}
+        </template>
+      </el-menu-item>
+    </router-link>
+  </template>
+  <el-sub-menu v-else ref="subMenu" :index="item.path">
     <template #title>
       <!-- TODO: Support w/ icon -->
-      {{ item.meta?.title }}
+      {{ item.meta?.title || item.name }}
       <!-- <item
 				v-if="item.meta"
 				:icon="item.meta && item.meta.icon"
